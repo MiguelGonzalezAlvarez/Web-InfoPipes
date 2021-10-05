37bcb20fd84df290ff5606c364f9f2ec60014341
MiguelMuros <miggonzalv@gmail.com>
2021-10-05 22:09:33 +0200

    feat: create ventas components and router module

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NumerosComponent } from './pages/numeros/numeros.component';
+import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
+import { ComunesComponent } from './pages/comunes/comunes.component';
+import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NumerosComponent,
+    NoComunesComponent,
+    ComunesComponent,
+    OrdenarComponent
+  ],
+  exports: [
+    NumerosComponent,
+    NoComunesComponent,
+    ComunesComponent,
+    OrdenarComponent
+  ],
   imports: [
     CommonModule
   ]
